fcfa3d6715a9a728dd185da5ddd8c5dca173cef4
ashusinghOO7 <76557299+ashusinghOO7@users.noreply.github.com>
2022-07-06 00:55:29 +0530

    Time: 2 ms (68.26%), Space: 44.6 MB (13.25%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> al = new ArrayList<>();
+    public void solver(int open , int close ,String str){
+        if(open==0 && close==0){
+            al.add(str);
+            return;
+        }
+        if(open!=0){
+            String str1 = str;
+            str1 += "(";
+            solver(open-1,close,str1);
+        }
+        if(open<close){
+            String str2 = str;
+            str2 += ")";
+            solver(open,close-1,str2);
+            return;
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+        solver(n,n,"");
+        return al;
+    }
+}
