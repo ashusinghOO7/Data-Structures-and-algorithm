d18b1f0b470597ec5ace403f6ff864d75fff3c77
Ashutosh kumar Singh <76557299+ashusinghOO7@users.noreply.github.com>
2022-07-26 19:53:07 +0530

    Time: 1502 ms (8.74%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -2,4 +2,4 @@
  Please write a DELETE statement and DO NOT write a SELECT statement.
  Write your PL/SQL query statement below
  */
-Delete from person p where p.id <> (Select Min(id) from person where email=p.email);
+Delete from person p where p.id not in (Select Min(id) from person group by email);
