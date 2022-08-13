1f58f11b16fc8eb5c669b10285207882a838549f
Ashutosh kumar Singh <76557299+ashusinghOO7@users.noreply.github.com>
2022-08-13 19:01:33 +0530

    Time: 2 ms (78.73%), Space: 42 MB (14.45%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int reverse(int x) {
+        int signMultiplier = 1;
+        if (x < 0) {
+            signMultiplier = -1;
+            x = signMultiplier * x;
+        }
+
+        int res = 0;
+        while (x > 0) {
+
+            // Check if the result is within MaxInt32 and MinInt32 bounds
+            if (res * signMultiplier > Integer.MAX_VALUE / 10 || res * signMultiplier < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            // Add the current digit into result
+            res = res * 10 + x % 10;
+
+            x = x / 10;
+        }
+        // Restore to original sign of number (+ or -)
+        return (int)(signMultiplier * res);
+    }
+}
