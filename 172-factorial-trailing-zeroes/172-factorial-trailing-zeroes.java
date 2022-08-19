77c936b53f56b4c3b79d267e0395d7855b3928db
Ashutosh kumar Singh <76557299+ashusinghOO7@users.noreply.github.com>
2022-08-20 01:43:07 +0530

    Time: 1 ms (88.08%), Space: 41.5 MB (28.14%) - LeetHub

---
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
@@ -0,0 +1,12 @@
+class Solution {
+    int res = 0;
+    public int helper(int n){
+        if(n<5){
+            return 0;
+        }
+        return n/5 + helper(n/5);
+    }
+    public int trailingZeroes(int n) {
+        return helper(n);
+    }
+}
