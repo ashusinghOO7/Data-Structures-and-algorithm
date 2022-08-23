ea693769e6dde816e152e992cfea4a8174f0222d
Ashutosh kumar Singh <76557299+ashusinghOO7@users.noreply.github.com>
2022-08-23 19:26:28 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (18.63%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.java
+++ 328-odd-even-linked-list/328-odd-even-linked-list.java
@@ -10,33 +10,18 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null || head.next ==null){
-            return head;
+        if (head != null) {
+    
+        ListNode odd = head, even = head.next, evenHead = even; 
+    
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next; 
+            even.next = even.next.next; 
+            odd = odd.next;
+            even = even.next;
         }
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode evnt = even;
-        boolean add=false;
-        ListNode temp = head.next.next;
-        ListNode res = odd;
-        odd.next=null;
-        even.next=null;
-        while(temp!=null){
-            if(add==true){
-                even.next = temp;
-                temp=temp.next;
-                even = even.next;
-                even.next=null;
-            }
-            else{
-                odd.next = temp;
-                temp=temp.next;
-                odd = odd.next;
-                odd.next=null;
-            }
-            add = !add;
-        }
-        odd.next = evnt;
-        return head;
+        odd.next = evenHead; 
+    }
+    return head;        
     }
 }
