eab5a0f812b44ca08db04e4182598cbd30df3795
Ashutosh kumar Singh <76557299+ashusinghOO7@users.noreply.github.com>
2022-08-24 01:20:32 +0530

    Time: 6 ms (23.37%), Space: 91.6 MB (72.60%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        int small = Integer.MAX_VALUE,big=Integer.MAX_VALUE;
+        for(int i:nums){
+            if(i<=small){
+                small=i;
+            }
+            else if(i<=big){
+                big=i;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
