97d8f5985ba68ca0987263bfb940a83cc41e59bd
Ashutosh kumar Singh <76557299+ashusinghOO7@users.noreply.github.com>
2022-08-31 19:48:38 +0530

    Time: 443 ms (97.73%), Space: 0B (100.00%) - LeetHub

--- 178-rank-scores/178-rank-scores.sql
+++ 178-rank-scores/178-rank-scores.sql
@@ -1,3 +1,5 @@
 /* Write your PL/SQL query statement below */
-Select score,(Select Count(Distinct score) from Scores where score>a.score)+1 rank
-from Scores a Order by score desc
+/*Select score,(Select Count(Distinct score) from Scores where score>a.score)+1 rank
+from Scores a Order by score desc*/
+select score, dense_rank() over(order by score desc) as rank
+from Scores
